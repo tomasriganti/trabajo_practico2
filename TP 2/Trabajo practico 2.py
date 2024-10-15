499b916416d1b37bea2c2728aa62ef9fcab669d6
JoaquinPanichelli <jpanichelli@udesa.edu.ar>
2024-10-15 19:24:13 -0300

    Update Trabajo practico 2.py

--- TP 2/Trabajo practico 2.py
+++ TP 2/Trabajo practico 2.py
@@ -7,36 +7,51 @@ from PIL import Image
 
 path = 'C:/Users/joaco/OneDrive/Desktop/UDESA/primer AÑO 1er CUATRI/Pensamiento Computacional/TPS/TP 2/castle.jpg'
 
-#esta funcion devuelve la foto en forma de matriz en blanco y negro
-def generar_matriz_en_gris(path):
-    matriz_gris = np.array(Image.open(path).convert("L"))
-    return matriz_gris
-
+#esta funcion devuelve la foto en forma de matriz con las 3 dimensiones (RGB)
+def generar_matriz_rgb(path) -> np.array:
+    matriz = np.array(Image.open(path))
+    return matriz
 
 np.set_printoptions(threshold=np.inf)
 
+matriz_RGB = generar_matriz_rgb(path)
 
-print(generar_matriz_en_gris(path))
+#medidas de la matriz_1
+print(f'matriz: {matriz_RGB.shape}')  
+          
 
-matriz_1 = generar_matriz_en_gris(path)
+#divido en 3 matrices, en RGB
+matriz_rojo = matriz_RGB[ :, :, 0]
+matriz_verde = matriz_RGB[ :, :, 1]
+matriz_azul = matriz_RGB[ :, :, 2]
 
-
-#filtro horizontal
-def sobel_horizontal(matriz_1):
-    sobel_x = np.array([[-1, 0, 1],[-2, 0, 2],[-1, 0, 1]])
-
-    matriz_sobel_x = 1
-
-
-    return matriz_sobel_x
-
-
+print(f'MATRIZ RED: {matriz_rojo.shape}')
+print(f'MATRIZ GREEN: {matriz_verde.shape}')
+print(f'MATRIZ BLUE: {matriz_azul.shape}')
 
 
-#filtro vertical
-def sobel_vertical(matriz_1):
-    sobel_y = np.array([[-1, -2, -1],[0, 0, 0],[1, 2, 1]])
+#-----------------------------------------------------------------------------------------------------------------------------------------------------------
 
-    matriz_sobel_y = 1
+def matriz_con_padding(matriz: np.array, pad_width: int = 1) -> np.array:
+    """
+    Esta función aplica padding a la matriz usando el modo 'edge',
+    replicando los bordes de la imagen original.
+    
+    :param matriz: La matriz a la que se le agregará padding.
+    :param pad_width: El tamaño del padding a agregar.
+    :return: La matriz con padding agregado.
+    """
+    matriz_padded = np.pad(matriz, pad_width=pad_width, mode='edge')
+    return matriz_padded
 
-    return matriz_sobel_y
+
+
+# Agregar padding a las matrices de color
+matriz_rojo_padded = matriz_con_padding(matriz_rojo)
+matriz_verde_padded = matriz_con_padding(matriz_verde)
+matriz_azul_padded = matriz_con_padding(matriz_azul)
+
+# Verificar las dimensiones de las matrices con padding
+print(f'MATRIZ RED con padding: {matriz_rojo_padded.shape}')
+print(f'MATRIZ GREEN con padding: {matriz_verde_padded.shape}')
+print(f'MATRIZ BLUE con padding: {matriz_azul_padded.shape}')
